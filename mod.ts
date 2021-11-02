9f401a56d587b6db7d16b8f07cb144613d53c09c
lucas <lucasfonsecard@icloud.com>
2021-11-02 02:20:00 -0300

    feat: add verbose flag and cheap logger

--- mod.ts
+++ mod.ts
@@ -1,40 +1,70 @@
-//! Small command-line utility for adding new entries to `.gitignore`
-///
-/// # Install:
+//! Small command-line utility for adding new entries to `.gitignore`.
 /// deno install --allow-read --allow-write mod.ts
+import * as ink from 'https://deno.land/x/ink@1.3/mod.ts';
+import { readLines } from "https://deno.land/std/io/buffer.ts";
 
-import * as ink from 'https://deno.land/x/ink@1.3/mod.ts'
+interface Arguments {
+    help: boolean;
+    version: boolean;
+    verbose: boolean;
+    files: string[];
+}
 
-const args = Deno.args;
+interface Flags {
+    [key: string]: boolean;
+}
 
-if (args.length == 0) {
-    printUsage();
-    Deno.exit(1);
+async function parseArgs(): Promise<Arguments>{
+    const list: string[] = [];
+    // Detect piped input
+    if (!Deno.isatty(Deno.stdin.rid)) {
+      for await (const line of readLines(Deno.stdin)) {
+        line && list.push(line);
+      }
+    }
+    const args = [...list, ...Deno.args];
+    const files = args.filter(arg => !arg.startsWith("-"));
+    const flags = args.filter(arg => arg.startsWith("-"));
+
+    const parsedFlags: Flags = flags.reduce((acc, flag) => {
+        const key = flag.replace(/^-*/, "");
+        acc[key] = true;
+        return acc;
+    }, {} as Flags);
+
+    return {
+        help: parsedFlags["help"] || parsedFlags["h"],
+        version: parsedFlags["version"],
+        verbose: parsedFlags["verbose"] || parsedFlags["v"],
+        files
+    };
 }
 
 function printUsage() {
+    console.log(ink.colorize(
+`gitignore v1.0.0
+Small command-line utility for adding new entries to .gitignore.
+This is free software, and you are welcome to redistribute it under the terms of the GPLv3 license.
+
+<yellow>USAGE:</yellow> <green>gitignore</green> <yellow>[options] <file></yellow>...
+
+<yellow>EXAMPLE:</yellow> <green>gitignore</green> <magenta><u>node_modules/</u></magenta> <yellow>\"*.out\"</yellow>
+
+<yellow>OPTIONS:</yellow>
+    -v, --verbose    Prints the files that are being added/skipped.
+    -h, --help       Prints this help message.
+        --version    Prints the version number.
+
+<yellow>NOTE:</yellow> <green>gitignore</green> will not add paths that already exist in .gitignore.`
+    ));
+}
+
+function printVersion() {
     console.log("gitignore v1.0.0");
-    console.log(ink.colorize("\n\t<yellow>USAGE:</yellow> gitignore [PATHS]..."));
-    console.log(ink.colorize("\n\t<yellow>EXAMPLE:</yellow> gitignore node_modules/ \"*.out\""));
-    console.log(ink.colorize("\n\t<yellow>NOTE:</yellow> <green>gitignore</green> will not add paths that already exist in .gitignore"));
+    console.log("GPLv3");
 }
 
-const encoder = new TextEncoder();
-const ignoredFiles = await getIgnoredFiles();
-
-const dedup = args.filter(file => {
-    if (ignoredFiles.some(ignoredFile => file === ignoredFile)) {
-        console.log(ink.colorize(`    <yellow><b>${file}</b></yellow> is already ignored, skipping...`));
-        return false;
-    }
-    return true;
-});
-
-for (const file of dedup) {
-    console.log(ink.colorize(`    <green>Adding</green> <yellow><b>${file}</b></yellow> to .gitignore`));
-    await Deno.writeFile(".gitignore", encoder.encode(file + "\n"), { append: true });
-}
-
+// Get the list of files that are already ignored
 async function getIgnoredFiles(): Promise<string[]> {
     try {
         const content = await Deno.readFile(".gitignore");
@@ -48,3 +78,62 @@ async function getIgnoredFiles(): Promise<string[]> {
     }
 }
 
+let VERBOSE = false;
+
+async function run() {
+    const args = await parseArgs();
+    if (args.help) {
+        printUsage();
+        Deno.exit(0);
+    }
+    if (args.version) {
+        printVersion();
+        Deno.exit(0);
+    }
+    if (args.verbose) {
+        VERBOSE = true;
+    }
+    if (args.files.length == 0) {
+        printUsage();
+        console.log(ink.colorize("\n<red>error:</red> No files specified"));
+        Deno.exit(1);
+    }
+
+    const encoder = new TextEncoder();
+    const ignoredFiles = await getIgnoredFiles();
+
+    const skipLog: string[] = []
+    const dedup = args.files.filter(file => {
+        if (ignoredFiles.some(ignoredFile => file === ignoredFile)) {
+            if (args.verbose) {
+                skipLog.push(file);
+            }
+            return false;
+        }
+        return true;
+    });
+
+    if (dedup.length == 0) {
+        console.log(ink.colorize("\n<yellow>No files to add</yellow>"));
+        Deno.exit(0);
+    }
+
+    log(formatLog(dedup, (file) => `<green><b>Adding:</b></green> <magenta>${file}</magenta> to .gitignore`));
+    log(formatLog(skipLog, (file) =>`<yellow>Skipping:</yellow> <magenta>${file}</magenta> is already ignored`));
+
+    await Deno.writeFile(".gitignore", encoder.encode(dedup.join("\n") + "\n"), { append: true });
+
+    console.log(ink.colorize(`<green><b>Done!</b></green> Added <green>${dedup.length}</green> new entries. Skipped <yellow>${skipLog.length}</yellow>.`));
+}
+
+function log(msg: string) {
+    if (VERBOSE) {
+        console.log(msg);
+    }
+}
+
+function formatLog(log: string[], format: (file: string) => string) {
+    return log.map(file => ink.colorize(`    ${format(file)}`)).join("\n");
+}
+
+run();

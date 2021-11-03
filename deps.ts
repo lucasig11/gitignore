e67d88e99a8f1bd36f1456e53600682a03889582
lucas <lucasfonsecard@icloud.com>
2021-11-03 05:55:41 -0300

    fix: ignore linting on imported function

--- deps.ts
+++ deps.ts
@@ -5,6 +5,7 @@ export { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
 export { Input } from "https://deno.land/x/cliffy@v0.20.1/prompt/input.ts";
 export { Toggle } from "https://deno.land/x/cliffy@v0.20.1/prompt/toggle.ts";
 
+// deno-lint-ignore camelcase
 import cache_dir from "https://deno.land/x/cache_dir@v0.1.1/mod.ts";
 
 export { cache_dir };

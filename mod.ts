860c72b11d360cc6c759ddeab9ce4ed6f52aebd8
lucas <lucasfonsecard@icloud.com>
2021-11-05 17:30:41 -0300

    fix: add `import.meta.main` guard to entry point

--- mod.ts
+++ mod.ts
@@ -4,22 +4,27 @@ import { parseArgs } from "./shared/args.ts";
 import { container, red } from "./deps.ts";
 
 import "./shared/container/mod.ts";
+import { Writer } from "./shared/util.ts";
 
-try {
-  const args = await parseArgs();
-  const cli = container.resolve(GitIgnoreCli);
-  await cli.run(args);
-} catch (e) {
-  if (e instanceof CliError) {
-    console.error(`${red("error:")} ` + e.message);
-    Deno.exit(e.exitCode);
-  } else {
-    console.error(
-      `${
-        red("error:")
-      } an unexpected error occurred. Please file an issue at https://github.com/lucasig11/gitignore/issues \n`,
-      red(e.toString()),
-    );
-    Deno.exit(1);
+if (import.meta.main) {
+  try {
+    const args = await parseArgs();
+    const cli = container.resolve(GitIgnoreCli);
+    await cli.run(args);
+  } catch (e) {
+    if (e instanceof CliError) {
+      Writer.writeAllSync(Deno.stderr, `${red("error:")} ` + e.message);
+      Deno.exit(e.exitCode);
+    } else {
+      Writer.writeAllSync(
+        Deno.stderr,
+        `${
+          red("error:")
+        } an unexpected error occurred. Please file an issue at https://github.com/lucasig11/gitignore/issues \n ${
+          red(e.toString())
+        }`,
+      );
+      Deno.exit(1);
+    }
   }
 }
